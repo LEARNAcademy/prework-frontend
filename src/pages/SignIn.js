4c955514c49e36ed9ebc1287cd246e2ff08cf650
sjproctor <sproctor950@gmail.com>
2020-04-26 20:31:28 -0700

    base set up

---
+++ src/pages/SignIn.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class SignIn extends Component{
+  render(){
+    return(
+      <React.Fragment>
+        <p>Sign in page</p>
+      </React.Fragment>
+    )
+  }
+}
+export default SignIn
