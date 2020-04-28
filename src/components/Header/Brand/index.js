8f38948ce9f25fc59daf0734f866e4a377283b6c
Art-Ortega <artortega@Arts-MacBook-Pro.local>
2020-04-28 15:36:15 -0700

    first upload front end

---
+++ src/components/Header/Brand/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Brand = () => {
+    return (
+        <>
+           <h2 className="brand">LEARN Academy Prework Application</h2>
+        </>
+    )
+}
+
+export default Brand
+
