c03c6359972955cd37088c893c38728af51ba62f
Art-Ortega <artortega@Arts-MacBook-Pro.local>
2020-05-01 09:31:37 -0700

    organized front end

--- src/components/LessonNav/index.js
+++ src/components/LessonNav/index.js
@@ -4,18 +4,18 @@ import './style.css'
 
 class LessonNav extends Component {
     
-    captureLesson(lesson){
-        this.props.currentLesson(lesson)
+    captureContent(content){
+        this.props.currentContent(content)
     }
 
     render() {
+        let {modules, lessons} = this.props
         return(
             <>
                 <aside style={{border:"solid black 3px"}}>
                     <div className="accordion" id="accordionExample">
                         {/* map through our modules*/}
-                        {/* start map*/}
-                        {this.props.module.map((mod, i) => {
+                        {modules.map((mod, i) => {
                             return (
                                 <div className="card">
                                     <div className="card-header" id={`heading${i}`}>
@@ -31,22 +31,25 @@ class LessonNav extends Component {
                                         <div className="card-body">
                                             {/* Display lesson list */}
                                             <ul className="module-list">
-                                                {this.props.lesson.map((l,i,arr)=> {
-                                                    {console.log("array",arr[i].completed)}
+                                                {/*mapping over lessons*/}
+                                                {lessons.map((l,i,arr)=> {
                                                     let flag = ""
-                                                    
+                                                    {/* if the current lesson isn't the first lesson, disable it by default*/}
                                                     if(i !== 0 ){
                                                         flag = "disabled"
                                                     }
+                                                    {/* checks to see if lessons code module id matches the current modules id */}
                                                     if(l.code_module_id === mod.id){
+                                                        {/* if this is the first lesson, or if it's completed, override flag and enable the lesson*/}
                                                         if(i == 0 || arr[i].completed){
                                                             flag = ''
                                                         }
+                                                        {/* if a previous lesson exists and its completed, override the disabled flag and enable current lesson*/}
                                                         if(i-1 !== -1 && arr[i-1].completed){
                                                             flag=''
                                                         }
                                                         return (
-                                                            <li key={i} className={`nav-list ${flag}`}  style={{opacity: l.completed? '.2':'',color:l.completed? 'green':''}} onClick={ ()=>this.captureLesson(l)}>{l.title}</li>
+                                                            <li key={i} className={`nav-list ${flag}`}  style={{opacity: l.completed? '.2':'',color:l.completed? 'green':''}} onClick={ ()=>this.captureContent(l)}>{l.title}</li>
                                                         ) 
                                                     }
                                                 })}
