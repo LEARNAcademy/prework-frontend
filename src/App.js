080a8e7ffefb702793172248f225e96ff253db3f
Art-Ortega <artortega@Arts-MacBook-Pro.local>
2020-05-12 09:52:57 -0700

    async

--- src/App.js
+++ src/App.js
@@ -22,71 +22,103 @@ class App extends React.Component {
     this.getResources();
   }
 
-  componentDidMount() {
-    this.getTopics();
-    this.getModules();
-    this.getLessons();
-    this.getQuestions();
-    this.getResources();
+  async getTopics(){
+    let response = await fetch('http://localhost:3000/topics');
+    if(response.status === 200){
+      let data = await response.json();
+      this.setState({topics:data})
+    }
+  }
+  async getModules(){
+    let response = await fetch('http://localhost:3000/code_modules');
+    let data = await response.json();
+    if (response.status === 200) {
+      this.setState({modules:data})
+    }
+  }
+  async getLessons(){
+    let response = await fetch('http://localhost:3000/lessons')
+    let data = await response.json();
+    if(response.status === 200){
+      this.setState({lessons:data})
+    }
+  }
+  async getQuestions(){
+    let response = await fetch('http://localhost:3000/questions')
+    let data = await response.json();
+    if(response.status === 200) {
+      this.setState({questions:data})
+    }
+  }
+  async getResources(){
+    let response = await fetch('http://localhost:3000/resources')
+    let data = await response.json();
+    if (response.status === 200) {
+      this.setState({resources:data})
+    }
   }
 
-  getTopics = () => {
-    fetch('http://localhost:3000/topics').then(response => {
-      if (response.status === 200) {
-        return response.json();
-      }
-    }).then(topicArr => {
-      this.setState({topics:topicArr});
-    });
-  };
 
-  getModules = () => {
-    fetch('http://localhost:3000/code_modules').then(response => {
-      if (response.status === 200) {
-        return response.json();
-      }
-    }).then(modulesArr => {
-      this.setState({modules:modulesArr});
-    })
-  }
+  // getTopics = () => {
+  //   fetch('http://localhost:3000/topics').then(response => {
 
-  getLessons = () => {
-    fetch('http://localhost:3000/lessons').then(response => {
-      if (response.status === 200) {
-        return response.json();
-      }
-    }).then(lessonArr => {
-      this.setState({lessons:lessonArr})
-    })
-  }
+  //     if (response.status === 200) {
+  //       return response.json();
+  //     }
+  //   }).then(topicArr => {
+  //     this.setState({topics:topicArr});
+  //   });
+  // };
 
-  getQuestions = () => {
-    fetch('http://localhost:3000/questions').then(response => {
-      if (response.status === 200) {
-        return response.json();
-      }
-    }).then(questionsArr => {
-      this.setState({questions:questionsArr})
-    })
-  }
+  // getModules = () => {
+  //   fetch('http://localhost:3000/code_modules').then(response => {
+  //     if (response.status === 200) {
+  //       return response.json();
+  //     }
+  //   }).then(modulesArr => {
+  //     this.setState({modules:modulesArr});
+  //   })
+  // }
 
-  getResources = () => {
-    fetch('http://localhost:3000/resources').then(response => {
-      if (response.status === 200) {
-        return response.json();
-      }
-    }).then(resourceArr => {
-      this.setState({resources:resourceArr})
-    })
-  }
+  // getLessons = () => {
+  //   fetch('http://localhost:3000/lessons').then(response => {
+  //     if (response.status === 200) {
+  //       return response.json();
+  //     }
+  //   }).then(lessonArr => {
+  //     this.setState({lessons:lessonArr})
+  //   })
+  // }
+
+  // getQuestions = () => {
+  //   fetch('http://localhost:3000/questions').then(response => {
+  //     if (response.status === 200) {
+  //       return response.json();
+  //     }
+  //   }).then(questionsArr => {
+  //     this.setState({questions:questionsArr})
+  //   })
+  // }
+
+  // getResources = () => {
+  //   fetch('http://localhost:3000/resources').then(response => {
+  //     if (response.status === 200) {
+  //       return response.json();
+  //     }
+  //   }).then(resourceArr => {
+  //     this.setState({resources:resourceArr})
+  //   })
+  // }
 
   render(){
-  const current_user = {"id":1,"email":"artortega.25@gmail.com","last_q": 7}
+  const current_user = {"id":1,"email":"artortega.25@gmail.com","last_q": 5}
   console.log("lessonsss", this.state.lessons)
+  console.log("modules", this.state.modules)
+  console.log("questions", this.state.questions)
+  console.log("resources", this.state.resources)
   const loggedIn = true;
   const {topics, modules, lessons, questions, resources} = this.state;
   let isAdmin = false;
-  console.log("current_user",current_user.id)
   return (
     // eslint-disable-next-line react/jsx-filename-extension
     <>
