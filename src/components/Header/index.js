2a7976bfbfbecf6dc2fd251536acd9a822aaf817
hschaen <hschaen@gmail.com>
2020-05-15 14:42:08 -0700

    Updated styles in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,26 +17,33 @@ class Header extends React.Component{
         let isAdmin = false;
         let {current_user} = this.props;
         let userExist = this.checkUser();
+        console.log(current_user)
         return(
             <Container>
             <Row>
                 <Col sm={12}>
-                    {/*Brand on the left */}
-                    <Brand />
-                </Col>
-            {isAdmin &&
-                <Col>
-                    <Button href="/admin">Admin</Button>
-                </Col>
-            }
-            {userExist && 
-                <Col >
-                    {/*Logged in user on the right */}
-                    <div className="text-right">
-                        <p>{current_user.email}</p>
+                    <div className="header-top-bar w-100">
+                        <div id="top-bar-content" className="float-right mt-3">
+                            <span className="mr-3"><span className="material-icons mr-1">call</span> (619) 940-7848</span>
+                            <span className="mr-3"><span className="material-icons mr-1">email</span> hello@learnacademy.org</span>
+                            <span className=""><span className="material-icons">person</span> <a href="#" class="login">{userExist?`Sign Out`:`Sign In`}</a></span>
+                        </div>
                     </div>
                 </Col>
+                <Col sm={12}>
+                    <Brand />
+                
+            {isAdmin &&
+                
+                    <Button className="float-right" href="/admin">Admin</Button>
+                
             }
+            {current_user.length !== 0 &&
+                    <div className="user-email text-right">
+                        <p>You're logged in as: <span className="strong">{current_user.email}</span></p>
+                    </div>
+            }
+                </Col>
             </Row>
         </Container>
         )
